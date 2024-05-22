65854b480e5f8c04ad6adf8e8869fbfc56b42e23
VanajKalyanSR <iamvanaj@gmail.com>
2024-05-22 11:10:54 +0530

    Update question4.py

--- question4.py
+++ question4.py
@@ -1,1 +1,7 @@
 #write a program to find the sum of digits of a given number'
+a=int(input("Enter a number:"))
+for i in ramge(len(a)+1):
+  remainder=remainder%10
+  a=a/10
+  sum+=remainder
+print(sum)
