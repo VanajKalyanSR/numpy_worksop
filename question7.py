06c757243b7aafeca191271e93b07aaaee5948ef
VanajKalyanSR <iamvanaj@gmail.com>
2024-05-22 11:18:29 +0530

    Update question7.py

--- question7.py
+++ question7.py
@@ -10,3 +10,18 @@ e=[3]
 
 # use append method to do this.
 
+a = [1, 2, 3]
+b = [4, 5, 6]
+c = [7, 5, 3, 2, 3]
+d = [2]
+e = [3]
+
+output = []
+output.append(a)
+output.append(b)
+nested_list = c.copy()
+nested_list.append(d.copy())
+nested_list[-1].append(e.copy())
+output.append(nested_list)
+
+print(output)
