4d533a9b83bba04255637969577c17d91b305697
VanajKalyanSR <iamvanaj@gmail.com>
2024-05-22 11:17:40 +0530

    Update question6.py

--- question6.py
+++ question6.py
@@ -1,1 +1,12 @@
 #write a program to find the maximum of two numbers
+def find_max(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+num1 = 10
+num2 = 20
+
+maximum = find_max(num1, num2)
+print(f"The maximum of {num1} and {num2} is {maximum}.")
